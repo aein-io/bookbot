22f14d6c63a5a48417213407c542eb6de241c219
aein <aaeinnor11@gmail.com>
2025-03-12 16:59:55 +0800

    update main.py for dynamic book inputs

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import sys
+from stats import get_word_count, get_char_count, sort_chars
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
     sorted_char_count = sort_chars(char_count)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in the document\n")
+    print("============ BOOKBOT ============")
+    print(f"Analyszing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
 
     for item in sorted_char_count:
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
-    print("--- End report ---")
+    print("============= END ===============")
 
 
 def get_book_text(path):
@@ -19,37 +30,5 @@ def get_book_text(path):
         return f.read()
 
 
-def get_word_count(text):
-    words = text.split()
-    return len(words)
-
-
-def get_char_count(text):
-    char_list = {}
-    lowered_text = text.lower()
-
-    for char in lowered_text:
-        if char.isalpha():
-            if char in char_list:
-                char_list[char] += 1
-            else:
-                char_list[char] = 1
-
-    return char_list
-
-
-def sort_key(dict):
-    return dict["num"]
-
-
-def sort_chars(char_dict):
-    sorted = []
-
-    for char in char_dict:
-        sorted.append({"char": char, "num": char_dict[char]})
-
-    sorted.sort(reverse=True, key=sort_key)
-    return sorted
-
-
-main()
+if __name__ == "__main__":
+    main()
